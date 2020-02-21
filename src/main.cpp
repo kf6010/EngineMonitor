2674b70c5148a37e72fa30bd64e6085e1a1f5efd
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2020-02-21 11:57:10 +0000

    Added flag to only send replies to messages

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ static void canReadTask(void);
 int main()
 {
     red = 0;
-    canInit(BD125000, false);/* Not in loopback mode*/
+    canInit(BD125000, false);   /* Not in loopback mode */
     pc.printf("EngineMonitor -- \n");
 
     schInit();
@@ -46,18 +46,30 @@ static void led1ToggleTask(void)
     red = 1 - red;
 }
 
+/* Flag to say message has been received.
+ * I can't use a semaphore here as the scheduler
+ * treats these functions as one-shot actions
+ * very similar to interrupt handlers
+ * I CAN'T wait in a scheduled task.
+ */
+bool received;
+
+
 void canWriteTask(void)
 {
 
     static canMessage_t txMsg = { 0x24, 8, 0, 0 };
     bool txOk;
 
-    // Transmit message on CAN 
-    txOk = canWrite(&txMsg);
-    if (txOk) {
-        txCount += 1;
-        txMsg.dataA = txCount;
-        txMsg.dataB = rxCount;
+    if (received) {
+        // Transmit message on CAN 
+        txOk = canWrite(&txMsg);
+        if (txOk) {
+            txCount += 1;
+            txMsg.dataA = txCount;
+            txMsg.dataB = rxCount;
+        }
+        received = false;
     }
 }
 
@@ -67,11 +79,12 @@ void canReadTask(void)
     static canMessage_t rxMsg;
 
     if (canReady()) {
-		rxCount += 1;
+        rxCount += 1;
         canRead(&rxMsg);
         pc.printf
             ("ID: %#lx LEN: %lx DATA_A: %#06lx DATA_B: %#06lx\n", rxMsg.id,
              rxMsg.len, rxMsg.dataA, rxMsg.dataB);
         rxCount += 1;
+        received = true;
     }
 }
